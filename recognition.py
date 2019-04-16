a1134709ea69f6b47500dcc4a0de706c7de526a4
ruby913 <rubric555@gmail.com>
2019-04-15 20:41:38 -0700

    Update

--- recognition.py
+++ recognition.py
@@ -1,3 +1,4 @@
+# This Python function take photos from the Webcam, detects and recognizes face from the machine learning model.
 import tensorflow as tf
 import cv2
 import dlib
@@ -5,9 +6,6 @@ import numpy as np
 import os
 import random
 import sys
-from sklearn.model_selection import train_test_split
-import os
-
 import time
 import greengrasssdk
 import platform
@@ -26,50 +24,6 @@ size = 64
 camera = PiCamera()
 rawCapture = PiRGBArray(camera)
 
-imgs = []
-labs = []
-
-def getPaddingSize(img):
-    h, w, _ = img.shape
-    top, bottom, left, right = (0,0,0,0)
-    longest = max(h, w)
-
-    if w < longest:
-        tmp = longest - w
-        left = tmp // 2
-        right = tmp - left
-    elif h < longest:
-        tmp = longest - h
-        top = tmp // 2
-        bottom = tmp - top
-    else:
-        pass
-    return top, bottom, left, right
-
-def readData(path , h=size, w=size):
-    for filename in os.listdir(path):
-        if filename.endswith('.jpg'):
-            filename = path + '/' + filename
-            img = cv2.imread(filename)
-            top,bottom,left,right = getPaddingSize(img)
-            img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=[0,0,0])
-            img = cv2.resize(img, (h, w))
-            imgs.append(img)
-            labs.append(path)
-
-readData(my_faces_path)
-readData(other_faces_path)
-imgs = np.array(imgs)
-labs = np.array([[0,1] if lab == my_faces_path else [1,0] for lab in labs])
-train_x,test_x,train_y,test_y = train_test_split(imgs, labs, test_size=0.05, random_state=random.randint(0,100))
-train_x = train_x.reshape(train_x.shape[0], size, size, 3)
-test_x = test_x.reshape(test_x.shape[0], size, size, 3)
-train_x = train_x.astype('float32')/255.0
-test_x = test_x.astype('float32')/255.0
-
-print('train size:%s, test size:%s' % (len(train_x), len(test_x)))
-batch_size = 128
-num_batch = len(train_x) // 128
 
 x = tf.placeholder(tf.float32, [None, size, size, 3])
 y_ = tf.placeholder(tf.float32, [None, 2])
@@ -97,13 +51,9 @@ def dropout(x, keep):
 def cnnLayer():
     W1 = weightVariable([3,3,3,32]) 
     b1 = biasVariable([32])
-
     conv1 = tf.nn.relu(conv2d(x, W1) + b1)
-
     pool1 = maxPool(conv1)
-
     drop1 = dropout(pool1, keep_prob_5)
-
 
     W2 = weightVariable([3,3,32,64])
     b2 = biasVariable([64])
